a39129cf39b1b57e03420a61844471167752e0b0
david <davidoregan91@gmail.com>
2014-06-03 21:14:42 +0100

    Reg

--- Web/android_login_api/login.php
+++ Web/android_login_api/login.php
@@ -1,4 +1,105 @@
 <?php
+ 
+ str = "Made it!";
+
+$link = mysql_connect( '192.168.100.101:3306', 'david', 'apUJP5VxBTZ9atXD' );
+echo $str;
+if ( !$link ) {
+  die( 'Could not connect: ' . mysql_error() );
+}
+
+// Select the data base
+$db = mysql_select_db( 'david', $link );
+echo $str;
+if ( !$db ) {
+  die ( 'Error selecting database \'david\' : ' . mysql_error() );
+}
+ 
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+        $result = mysql_query("INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at) VALUES('$uuid', '$name', '$email', '$encrypted_password', '$salt', NOW())");
+        // check for successful store
+        if ($result) {
+            // get user details 
+            $uid = mysql_insert_id(); // last inserted id
+            $result = mysql_query("SELECT * FROM users WHERE uid = $uid");
+            // return user details
+            return mysql_fetch_array($result);
+        } else {
+            return false;
+        }
+    }
+ 
+    /**
+     * Get user by email and password
+     */
+    public function getUserByEmailAndPassword($email, $password) {
+        $result = mysql_query("SELECT * FROM users WHERE email = '$email'") or die(mysql_error());
+        // check for result 
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            $salt = $result['salt'];
+            $encrypted_password = $result['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+                return $result;
+            }
+        } else {
+            // user not found
+            return false;
+        }
+    }
+ 
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($email) {
+        $result = mysql_query("SELECT email from users WHERE email = '$email'");
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            // user existed 
+            return true;
+        } else {
+            // user not existed
+            return false;
+        }
+    }
+ 
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+ 
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+ 
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkhashSSHA($salt, $password) {
+ 
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+ 
+        return $hash;
+    }
 /**
  * File to handle all API requests
  * Accepts GET and POST
@@ -9,6 +110,7 @@
   /**
  * check for POST request 
  */
+
 if (isset($_POST['tag']) && $_POST['tag'] != '') {
     // get tag
     $tag = $_POST['tag'];
