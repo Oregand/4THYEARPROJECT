07603936833e8af1980e5ec2fdaed4fe13b5d215
david <davidoregan91@gmail.com>
2014-06-03 21:04:03 +0100

    Reg

--- Web/android_login_api/DB_Connect.php
+++ Web/android_login_api/DB_Connect.php
@@ -1,17 +1,20 @@
 <?php
  
+class DB_Connect {
  
   $link = mysql_connect( '192.168.100.101:3306', 'david', 'apUJP5VxBTZ9atXD' );
-  echo "Made it into db"
+  echo "Made it into db";
 if ( !$link ) {
   die( 'Could not connect: ' . mysql_error() );
 }
 
 // Select the data base
 $db = mysql_select_db( 'david', $link );
-echo "double made it"
+echo "Im the best";
 if ( !$db ) {
   die ( 'Error selecting database \'david\' : ' . mysql_error() );
+}
+
 }
  
 ?>
